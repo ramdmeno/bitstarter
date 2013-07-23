9df3e333cb2ea38d8f5fa6ac4beba65292cabe99
Ubuntu <ubuntu@ip-10-164-99-75.ec2.internal>
2013-07-23 01:55:27 +0000

    update

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/images'));
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html', 'utf8');   
